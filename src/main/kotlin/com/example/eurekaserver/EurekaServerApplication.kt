1362c7ba7e104e04d4055a39c7560d1b10a8a2fe
furkandel <57002551+Furkan-del@users.noreply.github.com>
2023-08-13 02:02:49 +0300

    bugfix

--- src/main/kotlin/com/example/eurekaserver/EurekaServerApplication.kt
+++ src/main/kotlin/com/example/eurekaserver/EurekaServerApplication.kt
@@ -2,10 +2,11 @@ package com.example.eurekaserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class EurekaServerApplication
-
 fun main(args: Array<String>) {
 	runApplication<EurekaServerApplication>(*args)
 }
